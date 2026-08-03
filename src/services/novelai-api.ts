d4081a16d0d816a667dba97bd9caed73119df39a
IZTACIYU <joey020830@gmail.com>
2026-08-03 23:12:14 +0900

    Test uncond scale generation parameter

--- src/services/novelai-api.ts
+++ src/services/novelai-api.ts
@@ -528,6 +528,7 @@ export async function generateImage(
             scale: params.cfg_scale,
             negative_prompt: params.negative_prompt,
             cfg_rescale: params.cfg_rescale,
+            uncond_scale: 1,
             noise_schedule: params.scheduler,
 
             // Version and legacy settings
@@ -1085,6 +1086,7 @@ export async function generateImageStream(
             scale: params.cfg_scale,
             negative_prompt: params.negative_prompt,
             cfg_rescale: params.cfg_rescale,
+            uncond_scale: 1,
             noise_schedule: params.scheduler,
 
             // Version and legacy settings
